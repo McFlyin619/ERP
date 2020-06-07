166ad9c7815f59bcef6c024168ccf3b4a0bbe631
OBrien McQuade <62272369+McFlyin619@users.noreply.github.com>
2020-06-06 20:46:08 -0700

    SCSS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { FormsModule} from '@angular/forms';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
